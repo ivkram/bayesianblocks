60dee0672da7b1e0546e89e4c0523c6727f508a2
Ivan Kramarenko <im.kramarenko@gmail.com>
2019-10-16 01:36:43 +0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,14 +1,8 @@
 import glob
 import os
 
-path = '*.png'
+path = 'r vs g light curves (bayesian)/'+ '*.png'
 files = glob.glob(path)
 for src in files:
     os.remove(src)
-
-path = '*_bb'
-files = glob.glob(path)
-for src in files:
-    os.remove(src)
-
     
